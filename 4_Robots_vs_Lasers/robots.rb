2fa5138a1c5ddcc8477168364e8c17bf32b1f4b1
eregon <eregontp@gmail.com>
2011-03-30 23:58:11 +0200

    enhance beauty

--- 4_Robots_vs_Lasers/robots.rb
+++ 4_Robots_vs_Lasers/robots.rb
@@ -1,36 +1,35 @@
-Input = ARGV.delete('-s') ? 'sample-input.txt' : 'input.txt'
+# Robots vs Lasers
+
+# This is mainly a tight embrace of curly and square braces
+
+Input = 'input.txt'
+Input.prepend('sample-') if ARGV.delete('-s')
 
 Directions = [:west, :east]
 
-def best_way(lasers, start)
-  conveyor_size = lasers.first.size
-  positions = { west: start, east: start }
+def destiny(lasers, start)
+  positions = Hash.new(start)
   damages = Hash.new(0)
-  conveyor_exits = { west: 0, east: conveyor_size-1 }
-  
+  exits = { west: 0, east: lasers.first.size-1 }
+
   other = -> d { Directions.find { |dir| dir != d } }
   go = -> d { "GO #{d.upcase}" }
 
-  [start, conveyor_size-start-1].max.times { |click|
+  [exits[:west]+start, exits[:east]-start].max.times { |click|
     { west: -1, east: 1 }.each_pair { |direction, step|
       position = positions[direction]
-      if position == conveyor_exits[direction]
-        if damages[other[direction]] > damages[direction]
-          return go[direction]
-        end
+      if position == exits[direction]
+        return go[direction] if damages[other[direction]] > damages[direction]
       else
         damages[direction] += 1 if lasers[click % 2][position]
-
         positions[direction] += step
       end
     }
   }
-  go[Directions.min_by { |d| damages[d] }]
+  go.call Directions.min_by { |d| damages[d] }
 end
 
 File.readlines(Input).map(&:chomp).reject(&:empty?).each_slice(3) { |north, robot, south|
   lasers = [north, south].map { |side| side.chars.map { |c| c == '|' } }
-  #p [north, robot, south]
-  #p lasers
-  puts best_way(lasers, robot.index('X'))
+  puts destiny(lasers, robot.index('X'))
 }
