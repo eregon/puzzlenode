176c8839455b21255b7088153c0e1d74fbd325bb
eregon <eregontp@gmail.com>
2011-05-26 19:21:31 +0200

    abstract code in a Module, propose a nice API

--- 1_International_Trade/international_trade_solution.rb
+++ 1_International_Trade/international_trade_solution.rb
@@ -1,61 +1,76 @@
 require 'csv'
 require 'nokogiri'
 
-trans_file, rates_file = 'TRANS.csv', 'RATES.xml'
+module InternationTrade
+  extend self
 
-ITEM = 'DM1182'
-CURRENCY = :USD
+  def sum(item, in_currency, transactions_csv, rates_xml)
+    rates = parse_rates(rates_xml)
 
-RATES = Nokogiri::XML(open(rates_file)).slop!.rates.rate.each_with_object({}) { |rate, rates|
-  from, to = [:from, :to].map { |child| rate.send(child).text.to_sym }
-  rates[from] ||= { from => Rational(1) }
-  rates[from].merge! to => Rational(rate.conversion.text)
-}
-
-CURRENCIES = RATES.inject([]) { |currencies, (from, to_conversion)|
-  currencies | [from, *to_conversion.keys]
-}
-
-CURRENCIES.each { |currency|
-  RATES[currency] ||= {}
-  CURRENCIES.each { |cur|
-    RATES[currency][cur] = 1 / RATES[cur][currency] if RATES[cur][currency] and !RATES[currency][cur]
-  }
-}
-
-def find_conversion from, to
-  until RATES[from][to]
-    cur = RATES[from].keys.find(lambda {
-      raise "Couldnt find from #{from} to #{to} with #{values} and #{RATES}"
-    }) { |currency|
-      !(RATES[currency].keys - RATES[from].keys).empty?
+    currencies = rates.inject([]) { |currencies, (from, to_conversion)|
+      currencies | [from, *to_conversion.keys]
     }
-    RATES[cur].each_pair { |inter, val|
-      RATES[from][inter] = RATES[from][cur] * val
+
+    transactions = parse_transactions(transactions_csv, item)
+
+    round transactions.inject(0) { |total, (price, currency)|
+      total + round(price * find_conversion(rates, currency, in_currency))
     }
   end
-  RATES[from][to]
+
+  private
+  def parse_rates(xml)
+    Nokogiri::XML(open(xml)).slop!.rates.rate.each_with_object({}) { |rate, rates|
+      from, to = [:from, :to].map { |child| rate.send(child).text.to_sym }
+      rates[from] ||= { from => Rational(1) }
+      rates[from].merge! to => Rational(rate.conversion.text)
+    }
+  end
+
+  def parse_transactions(csv, item_searched)
+    transactions = []
+    CSV.foreach(csv) { |_, item, price|
+      next unless item == item_searched
+      price, currency = price.split(' ')
+      transactions << [Rational(price), currency.to_sym]
+    }
+    transactions
+  end
+
+  def find_rates_from_reverse(rates, currencies)
+    currencies.each { |currency|
+      rates[currency] ||= {}
+      currencies.each { |cur|
+        rates[currency][cur] = 1 / rates[cur][currency] if rates[cur][currency] and !rates[currency][cur]
+      }
+    }
+  end
+
+  def find_conversion rates, from, to
+    until rates[from][to]
+      cur = rates[from].keys.find(lambda {
+        raise "Couldnt find from #{from} to #{to} with #{values} and #{rates}"
+      }) { |currency|
+        !(rates[currency].keys - rates[from].keys).empty?
+      }
+      rates[cur].each_pair { |inter, val|
+        rates[from][inter] = rates[from][cur] * val
+      }
+    end
+    rates[from][to]
+  end
+
+  def round(r)
+    fix, frac = (100*r).divmod(1)
+    if frac == 0.5
+      [fix, fix + 1].find(&:even?)
+    elsif frac > 0.5
+      fix + 1
+    else
+      fix
+    end / 100.0
+  end
 end
 
-def round(r)
-  fix, frac = (100*r).divmod(1)
-  if frac == 0.5
-    [fix, fix + 1].find(&:even?)
-  elsif frac > 0.5
-    fix + 1
-  else
-    fix
-  end / 100.0
-end
-
-# Utica,DM1210,13.12 CAD
-
-data = []
-CSV.foreach(trans_file) { |_, item, price|
-  next unless item == ITEM
-  price, currency = price.split(' ')
-  data << [Rational(price), currency.to_sym]
-}
-
-p round data.inject(0) { |total, (price, currency)| total + round(price * find_conversion(currency, CURRENCY)) }
+p InternationTrade.sum('DM1182', :USD, 'TRANS.csv', 'RATES.xml')
 
