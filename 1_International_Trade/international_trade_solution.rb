765db3507fca436f672f5b7e57ea6762437cd63e
eregon <eregontp@gmail.com>
2011-05-26 18:59:57 +0200

    constants should be in SCREAMING_SNAKE_CASE

--- 1_International_Trade/international_trade_solution.rb
+++ 1_International_Trade/international_trade_solution.rb
@@ -3,42 +3,42 @@ require 'nokogiri'
 
 trans_file, rates_file = 'TRANS.csv', 'RATES.xml'
 
-Item = 'DM1182'
-Currency = :USD
+ITEM = 'DM1182'
+CURRENCY = :USD
 
-Rates = Nokogiri::XML(open(rates_file)).slop!.rates.rate.each_with_object({}) { |rate, rates|
+RATES = Nokogiri::XML(open(rates_file)).slop!.rates.rate.each_with_object({}) { |rate, rates|
   from, to = [:from, :to].map { |child| rate.send(child).text.to_sym }
   rates[from] ||= { from => Rational(1) }
   rates[from].merge! to => Rational(rate.conversion.text)
 }
 
-Currencies = Rates.inject([]) { |currencies, (from, to_conversion)|
+CURRENCIES = RATES.inject([]) { |currencies, (from, to_conversion)|
   currencies | [from, *to_conversion.keys]
 }
 
-Currencies.each { |currency|
-  Rates[currency] ||= {}
-  Currencies.each { |cur|
-    Rates[currency][cur] = 1 / Rates[cur][currency] if Rates[cur][currency] and !Rates[currency][cur]
+CURRENCIES.each { |currency|
+  RATES[currency] ||= {}
+  CURRENCIES.each { |cur|
+    RATES[currency][cur] = 1 / RATES[cur][currency] if RATES[cur][currency] and !RATES[currency][cur]
   }
 }
 
 def find_conversion(from, to)
-  Rates[from][to] or find_it!(from, to)
+  RATES[from][to] or find_it!(from, to)
 end
 
 def find_it! from, to
-  until Rates[from][to]
-    cur = Rates[from].keys.find(lambda {
-      raise "Couldnt find from #{from} to #{to} with #{values} and #{Rates}"
+  until RATES[from][to]
+    cur = RATES[from].keys.find(lambda {
+      raise "Couldnt find from #{from} to #{to} with #{values} and #{RATES}"
     }) { |currency|
-      !(Rates[currency].keys - Rates[from].keys).empty?
+      !(RATES[currency].keys - RATES[from].keys).empty?
     }
-    Rates[cur].each_pair { |inter, val|
-      Rates[from][inter] = Rates[from][cur] * val
+    RATES[cur].each_pair { |inter, val|
+      RATES[from][inter] = RATES[from][cur] * val
     }
   end
-  Rates[from][to]
+  RATES[from][to]
 end
 
 def round(r)
@@ -56,10 +56,10 @@ end
 
 data = []
 CSV.foreach(trans_file) { |_, item, price|
-  next unless item == Item
+  next unless item == ITEM
   price, currency = price.split(' ')
   data << [Rational(price), currency.to_sym]
 }
 
-p round data.inject(0) { |total, (price, currency)| total + round(price * find_conversion(currency, Currency)) }
+p round data.inject(0) { |total, (price, currency)| total + round(price * find_conversion(currency, CURRENCY)) }
 
