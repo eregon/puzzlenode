ca1edf584fd8c88e35c2bfef18504b47de6b2271
eregon <eregontp@gmail.com>
2011-06-01 17:13:21 +0200

    cleanup and make parse methods private

--- 6_Steganography_101/steganography.rb
+++ 6_Steganography_101/steganography.rb
@@ -1,40 +1,18 @@
 class Bitmap
   FILE_HEADER_SIZE = 14
   DIB_HEADER_SIZE = 40
-  
+
   def initialize(path)
     @path = path
     parse_header
     parse_pixel_array
   end
-  
-  def parse_header
-    header = File.binread(@path, FILE_HEADER_SIZE + DIB_HEADER_SIZE)
-    @pixel_array_offset = header[0xA, 4].unpack('L')[0]
-    
-    dib_header_size, @width, @height, _, @color_depth, compression, @pixel_array_size =
-      header[FILE_HEADER_SIZE, 4+4+4+2+2+4+4].unpack('Ll2SSLL')
-    raise "Incorrect DIB header size" unless dib_header_size == DIB_HEADER_SIZE
-    raise "Only handles 8-bit color depth" unless @color_depth == 8
-    raise "Compressed image" unless compression == 0
-  end
-  
-  def parse_pixel_array
-    pixel_array = File.binread(@path, @pixel_array_size, @pixel_array_offset)
-    @row_size = (@color_depth * @width / 32.0).ceil * 4
-    row_bytes = @color_depth * @width / 8
-    raise "Invalid image size" unless @pixel_array_size == @row_size * @height
 
-    @pixels = @height.times.map { |i|
-        pixel_array[i*@row_size, row_bytes].unpack('C*')
-    }
-  end
-  
   def encode_message(message)
     hidden_binary_message = message.unpack('C*').map { |i|
       i.to_s(2).rjust(8, '0')
     }.join.chars.map(&:to_i)
-    raise "Too long message" if hidden_binary_message.size > @width*@height
+    raise "Too long message" if hidden_binary_message.size > @width * @height
 
     @height.times { |y|
       @width.times { |x|
@@ -46,7 +24,7 @@ class Bitmap
       }
     }
   end
-  
+
   def pixel_array
     @height.times.with_object("") { |i, str|
       str << @pixels[i].pack('C*').tap { |row|
@@ -57,10 +35,34 @@ class Bitmap
 
   def save(path)
     header = File.binread(@path, @pixel_array_offset)
-    footer = File.binread(@path, nil, @pixel_array_offset+@pixel_array_size)
-    
+    footer = File.binread(@path, nil, @pixel_array_offset + @pixel_array_size)
+
     File.open(path, 'wb') { |file|
       file << header << pixel_array << footer
+    }
+  end
+
+  private
+  def parse_header
+    header = File.binread(@path, FILE_HEADER_SIZE + DIB_HEADER_SIZE)
+    @pixel_array_offset = header[0xA, 4].unpack('L')[0]
+
+    dib_header_size, @width, @height, _, @color_depth, compression, @pixel_array_size =
+      header[FILE_HEADER_SIZE, 4+4+4+2+2+4+4].unpack('Ll2SSLL')
+
+    raise "Incorrect DIB header size" unless dib_header_size == DIB_HEADER_SIZE
+    raise "Only handles 8-bit color depth" unless @color_depth == 8
+    raise "Compressed image" unless compression == 0
+  end
+
+  def parse_pixel_array
+    pixel_array = File.binread(@path, @pixel_array_size, @pixel_array_offset)
+    @row_size = (@color_depth * @width / 32.0).ceil * 4
+    row_bytes = @color_depth * @width / 8
+    raise "Invalid image size" unless @pixel_array_size == @row_size * @height
+
+    @pixels = @height.times.map { |i|
+        pixel_array[i*@row_size, row_bytes].unpack('C*')
     }
   end
 end
@@ -75,4 +77,3 @@ bitmap = Bitmap.new('input.bmp')
 message = File.read(input)
 bitmap.encode_message(message)
 bitmap.save(output)
-
